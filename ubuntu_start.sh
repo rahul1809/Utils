00235d4de042b640e0b1e4a49adf40be24e6ff84
rahul <rahul1809>
2017-06-30 18:47:24 +0530

    modified script for complete installation

--- ubuntu_start.sh
+++ ubuntu_start.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
 
-# SCRIPT FOR INITIAL UBUNTU SETUP
-# http://www.binarytides.com/better-ubuntu-15-10/
+#Author: Rahul Mishra
+#Email: rahul.mishra2003@gmail.com
+#Description: Complete Development Setup Script on Ubuntu Machine
+
+
+
 
 # Enable canonical repos
 sed -i.bak "/^# deb [^cdrom].*/ s/^# //" /etc/apt/sources.list
 
+
+
+#add all the repositories here.
+add-apt-repository ppa:shutter/ppa -y
+add-apt-repository ppa:webupd8team/sublime-text-3 -y
+add-apt-repository ppa:damien-moore/codeblocks-stable -y
+add-apt-repository ppa:webupd8team/java -y
+add-apt-repository ppa:me-davidsansome/clementine -y
+
+
 # update the system
 apt-get update
 
-# upgrade the system
-apt-get upgrade
+
+packages="synaptic gdebi-core ubuntu-wallpapers-* ubuntukylin-wallpapers-* ubuntustudio-wallpapers adobe-flashplugin  ubuntu-restricted-extras libavcodec-extra vlc unace rar unrar p7zip-rar p7zip sharutils uudeview mpack arj cabextract lzip lunzip plzip guake guake-indicator shutter sublime-text-installer samba codeblocks git oracle-java8-installer oracle-java8-set-default eclipse clementine  python2.7 python-pip python-dev  ipython ipython-notebook jupyter"
+
+# Installing starting packages
+echo "******************************************************"
+echo "               Installing Packages                    "
+echo "******************************************************"
+apt-get install -y $packages
+
+
+
+
+
+echo "******************************************************"
+echo "               Upgrading the System                   "
+echo "******************************************************"
+# finally upgrade the system and get distribution upgrade
+apt-get upgrade -y && apt-get dist-upgrade -y
+
+
+#cleaning up the system
+echo "******************************************************"
+echo "               CleaningUp the System                  "
+echo "******************************************************"
+
+apt-get autoremove -y
 
 # Disable apport
 sed -i.bak "s/^enabled=[01]$/enabled=0/" /etc/default/apport
 service apport restart
 
-# Install package installers
-apt-get install -y synaptic gdebi-core
 
-# Install Ubuntu Wallpapers
-apt-get install -y ubuntu-wallpapers-* ubuntukylin-wallpapers-* ubuntustudio-wallpapers
 
-# Install Chromium-Browser
-apt-get install -y chromium-browser
-
-# Chrome Browser 64 bit Download and Install
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi google-chrome-stable_current_amd64.deb &
-
-# Install Flash
-apt-get install -y adobe-flashplugin
-
-# Install multimedia codecs
-apt-get install -y ubuntu-restricted-extras libavcodec-extra
-
-# Install VLC Player
-add-apt-repository ppa:videolan/master-daily
-apt-get update
-apt-get install -y vlc
 
 # Install Dropbox
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd &
 
-# Install archive utilities
-apt-get install -y unace rar unrar p7zip-rar p7zip sharutils uudeview mpack arj cabextract lzip lunzip plzip
-
-# Install Guake
-apt-get install -y guake guake-indicator
-
-# Install shutter
-add-apt-repository ppa:shutter/ppa
-apt-get update
-apt-get install -y shutter
 
